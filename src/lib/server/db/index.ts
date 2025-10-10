13bb9f0e9976830c65b7eca278f512fa0e805aff
Brent Abrazaldo <92303760+Brent001@users.noreply.github.com>
2025-10-11 01:24:48 +0800

    Update index.ts

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,12 +1,10 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 import * as schema from './schema/schema.js';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const pool = new Pool({
-  connectionString: env.DATABASE_URL,
-});
+const client = neon(env.DATABASE_URL);
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(client, { schema });
